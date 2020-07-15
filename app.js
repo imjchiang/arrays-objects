28cd95f83e3746d006b10a440d149dad4b41d43d
imjchiang <jchiang296@gmail.com>
2020-07-15 14:03:51 -0700

    [arrays] add favorite teams

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Josh";
+console.log(name);
+
+var myLocation = "Los Angeles";
+console.log(myLocation);
+
+var favoriteNumber = 5;
+console.log(favoriteNumber);
+
+var teams = ["Rockets", "Lakers", "Warriors", "Clippers", "Blazers", "Celtics", "Nets"];
+console.log(teams);
