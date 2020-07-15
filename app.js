c8de0af96669488c99ab849d1ab7e5e346724173
imjchiang <jchiang296@gmail.com>
2020-07-15 14:46:33 -0700

    [loops] add for loops

--- app.js
+++ app.js
@@ -36,3 +36,12 @@ while (joshFav < toHundred)
     joshFav++;
 }
 
+for (var i = 0; i < 10; i++)
+{
+    console.log(i);
+}
+
+for (var i = 0; i < teams.length; i++)
+{
+    console.log(teams[i]);
+}
