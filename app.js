06c46669451ff5b8e0183af42b1bb21fe06f180d
imjchiang <jchiang296@gmail.com>
2020-07-15 16:33:12 -0700

    [functions] add numbers in array function

--- app.js
+++ app.js
@@ -186,3 +186,20 @@ function lessThanFifty(array)
 }
 
 lessThanFifty(otherNumbers);
+
+function addNums(array)
+{
+    let result = 0;
+
+    for (let i = 0; i < array.length; i++)
+    {
+        result += array[i];
+    }
+    
+    return result;
+}
+
+console.log(addNums(otherNumbers));
+
+let myResult = addNums(numbers);
+console.log(myResult);
